57039ecb8f1cfd9c7a759c72c6508b52748edd59
Sevval <Sevval@SA>
2021-12-04 04:01:22 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] numara;
+        V[] isim;
+        public MyDictionary()
+        {
+            numara = new K[0];      //constructor çalıştığında dizi tanımlanıcak
+            isim = new V[0];
+        }
+
+        public void Add(K _numara, V _isim)
+        {
+            K[] tempKey = numara;       //key için geçici dizi tanımlandı
+            numara = new K[numara.Length + 1];        //eklenecek anahtar için eleman sayısı 1 arttırıldı
+            for (int i = 0; i < tempKey.Length; i++)        //anahtarlar geçici diziden alındı
+            {
+                numara[i] = tempKey[i];
+            }
+            numara[numara.Length - 1] = _numara;        //son elemana istenilen değer eklendi
+
+            V[] tempValue = isim;
+            isim = new V[isim.Length + 1];
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                isim[j] = tempValue[j];
+            }
+            isim[isim.Length - 1] = _isim;
+        }
+
+        public void Yazdır()        //anahtar ve değerleri birleştirip görüntülemek içiin yazdır metodu 
+        {
+            for (int i = 0; i < numara.Length; i++)
+            {
+                Console.WriteLine(numara[i] + "-" + isim[i]);
+            }
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Add(15, "dsdadsasd");
+            ogrenciler.Add(18, "epwepweopwe");
+            ogrenciler.Yazdır();
+        }
+    }
+}
